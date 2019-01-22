68782fee8a62c9e5e244c7289a442fa7c980d044
Matthew Phelps <mphelps3@nd.edu>
2019-01-22 00:36:41 -0500

    gitignore and found error: gave incorrect permissions to write

--- project1/filecopy.c
+++ project1/filecopy.c
@@ -28,4 +28,35 @@ int main(int argc, char* argv[]){
 		fprintf(stderr, "%s: couldn't open %s: %s.\n", argv[0], dest, strerror(errno));
 		return EXIT_FAILURE;
 	}
+
+	// filecopy from src to dest
+	char buf[BUFSIZ];
+	int nread;
+
+	while ((nread = read(rfd, buf, BUFSIZ)) != 0){
+		if (nread < 0){
+			fprintf(stderr, "%s: failed reading %s: %s.\n", argv[0], src, strerror(errno));
+			return EXIT_FAILURE;
+		}
+		int nwritten = write(wfd, buf, nread);
+		if (nwritten < 0){
+			fprintf(stderr, "%s: FAILED writing to %s: %s.\n", argv[0], dest, strerror(errno));
+			fprintf(stderr, "Read %d written %d\n",nread,nwritten);
+			return EXIT_FAILURE;
+		}
+		// finish writes that take over a chunk
+		int nextwrites;
+		while (nwritten != nread){
+			if ((nextwrites = write(wfd, buf + nwritten, nread - nwritten)) < 0){
+				fprintf(stderr, "%s: failed writing to %s: %s.\n", argv[0], dest, strerror(errno));
+				return EXIT_FAILURE;
+			}
+			nwritten += nextwrites;
+		}
+	}
+
+	// finish
+	close(wfd);
+	close(rfd);
+	return EXIT_SUCCESS;
 }
