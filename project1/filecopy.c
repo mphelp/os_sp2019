b6c1cb91592df8e3036008a2e483b08241df71fc
Matthew Phelps <mphelps3@nd.edu>
2019-01-22 00:06:42 -0500

    arg parsing and opening

--- project1/filecopy.c
+++ project1/filecopy.c
@@ -1,6 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(){
-	printf("Hello world!\n");
+int main(int argc, char* argv[]){
+	if (argc != 3){
+		fprintf(stderr, "Usage: %s [src] [dest]\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	char* src = argv[1];
+	char* dest = argv[2];
+
+	// open src
+	int rfd = open(src, O_RDONLY);
 }
