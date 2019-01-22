ed2c3b218b6b7cd8b957af049da336532d45d1c9
Matthew Phelps <mphelps3@nd.edu>
2019-01-22 14:33:01 -0500

    progress

--- project1/treecopy.c
+++ project1/treecopy.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+
+/* Error msgs */
+void err(char* message, char* file, char* program){
+	fprintf(stderr, "%s: %s: %s.\n", program, message, file);
+}
 
 int main(int argc, char* argv[]){
 	// arg parsing
@@ -19,10 +25,10 @@ int main(int argc, char* argv[]){
 	// open src dir
 	DIR* sdir = opendir(srcDir);
 	if (sdir == NULL){
-		fprintf(stderr, "%s: Unable to open dir %s: %s.\n", argv[0], srcDir, strerror(errno));
+		/* fprintf(stderr, "%s: Unable to open dir %s: %s.\n", argv[0], srcDir, strerror(errno)); */
+		err("Unable to open dir", srcDir, argv[0]);
 		return EXIT_FAILURE;
 	}
-	printf("%d\n",sdir);
 	// make dest dir
 	int wfd = mkdir(destDir, 0777);
 	if (wfd < 0){
@@ -31,6 +37,10 @@ int main(int argc, char* argv[]){
 	}
 
 	/* Cleanup */
+	if (closedir(sdir) < 0){
+		fprintf(stderr,"%s: Couldn't close src dir %s: %s.\n", argv[0], destDir, strerror(errno));
+		return EXIT_FAILURE;
+	}
 	close(wfd);
 	return EXIT_SUCCESS;
 }
