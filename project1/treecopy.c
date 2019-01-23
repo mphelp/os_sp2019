c2379ba0a998a486a26d24d38c92e5e6e7fcbcea
Matthew Phelps <mphelps3@nd.edu>
2019-01-22 21:27:58 -0500

    lets do this

--- project1/treecopy.c
+++ project1/treecopy.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define eqstr(s1, s2) (strcmp((s1),(s2)) == 0)
 
@@ -15,7 +16,52 @@ void err(char* message, const char* file){
 	fprintf(stderr, "%s: %s: %s.\n", PROGRAM, message, file);
 }
 
-int walk(const char* root){
+int fileCopy(char* src, char* dest){
+	// open src/dest
+	int rfd = open(src, O_RDONLY);
+	if (rfd < 0){
+		err("Couldn't open src", src);
+		return EXIT_FAILURE;
+	}
+	int wfd = open(dest, O_WRONLY | O_CREAT);
+	if (wfd < 0){
+		err("Couldn't open dest", dest);
+		return EXIT_FAILURE;
+	}
+
+	// filecopy
+	char buf[BUFSIZ];
+	int nread, nwritten;
+	
+	while ((nread = read(rfd, buf, BUFSIZ)) != 0){
+		if (nread < 0){
+			err("Failed reading src", src);
+			return EXIT_FAILURE;
+		}
+		nwritten = write(wfd, buf, nread);
+		if (nwritten < 0){
+			err("FAILED writing to dest", dest);
+			/* fprintf(stderr, "Read %d written %d\n",nread,nwritten); */
+			return EXIT_FAILURE;
+		}
+		// finish writes that take more than a chunk
+		int nextwrites;
+		while (nwritten != nread){
+			if ((nextwrites = write(wfd, buf + nwritten, nread - nwritten)) < 0){
+				err("FAILED writing to dest", dest);
+				return EXIT_FAILURE;
+			}
+			nwritten += nextwrites;
+		}
+	}
+
+	// finish
+	close(wfd);
+	close(rfd);
+	printf("copied %d bytes from %s to %s\n", nwritten, src, dest);
+	return EXIT_SUCCESS;
+}
+int walkDir(const char* root){
 	// open root
 	DIR* dir = opendir(root);
 	if (dir == NULL){
@@ -35,7 +81,7 @@ int walk(const char* root){
 
 		// recurse
 		if (e->d_type == DT_DIR){
-			walk(path);
+			walkDir(path);
 		}	
 	}
 	// finish by closing
@@ -67,8 +113,8 @@ int main(int argc, char* argv[]){
 	}
 
 
-	// walk and copy src dir
-	if (walk(srcDir) < 0){
+	// walk dir and copy src dir
+	if (walkDir(srcDir) < 0){
 		return EXIT_FAILURE;
 	}
 
