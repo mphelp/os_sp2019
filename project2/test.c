3b2ff705a77cd8d13c0d94fc6541a413f40afab1
Matthew Phelps <mphelps3@nd.edu>
2019-01-29 15:01:34 -0500

    testing fgets

--- project2/test.c
+++ project2/test.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 #define MAX_CHARACTER_INPUT 1000
 #define MAX_DISTINCT_WORDS 	100
+#define BUF_CUSTOM 					10
 
 int main(int argc, char* argv[]){
+	// Read from line
+	char buf[BUF_CUSTOM];
+	char* text = calloc(1,1);
+	printf("Enter message: ");
+	while (fgets(buf, BUF_CUSTOM, stdin)){
+		text = realloc(text, strlen(text)+strlen(buf)+1);
+		
+		strcat(text, buf);
+		printf("%s\n",buf);
+	}
+	printf("\nAll text:%s",text);
+
 	return EXIT_SUCCESS;
 }
