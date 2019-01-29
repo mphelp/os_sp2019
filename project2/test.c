1c3e8e35ad773e1c7f072c21f95da1fdb5f453a0
Matthew Phelps <mphelps3@nd.edu>
2019-01-29 18:56:48 -0500

    adding err funcs and helpers

--- project2/test.c
+++ project2/test.c
@@ -5,12 +5,28 @@
 #define MAX_CHARACTER_INPUT 1000
 #define MAX_DISTINCT_WORDS 	100
 
+// errors
+#define debug(M, ...)\
+	fprintf(stderr, "%s: " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define userError(M, ...)\
+	fprintf(stderr, "%s: " M "\n", __FILE__, ##__VA_ARGS__)
+
 // helper functions
 void printWords(char* words[]){
 	for (int w = 0; words[w] != NULL; w++){
 		printf("%s ",words[w]);
 	}
 	printf("\n");
+}
+void parseWordsFromLine(char line[], char* words[]){
+	words[0] = strtok(line," \t\n");
+	int nwords = 1;
+	while ((words[nwords] = strtok(0, " \t\n")) != NULL){
+		if (nwords == MAX_DISTINCT_WORDS)
+			break;
+		nwords++;
+	}
+	words[nwords] = 0; // null terminated
 }
 
 int main(int argc, char* argv[]){
@@ -19,21 +35,14 @@ int main(int argc, char* argv[]){
 	char* words[MAX_DISTINCT_WORDS];
 
 	printf("Enter message: ");
-
-	while (fgets(line, MAX_CHARACTER_INPUT, stdin)){
-		
-		words[0] = strtok(line," \t\n");
-		int nwords = 1;
-		while ((words[nwords] = strtok(0, " \t\n")) != NULL){
-			if (nwords == MAX_DISTINCT_WORDS){
-				break;
-			}
-			nwords++;
-		}
-		words[nwords] = 0; 
+	char* line = fgets(line, MAX_CHARACTER_INPUT, stdin);
+	if (line == NULL){
+		userError("User input retrieval failed");
+		return EXIT_FAILURE;
 	}
-	// Print each word
-	printWords(words);
+	while ()
+	
+	
 
 	return EXIT_SUCCESS;
 }
