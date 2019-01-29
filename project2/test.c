475d7ad9175b4caf12f40141194bdd9e11b0ade6
Matthew Phelps <mphelps3@nd.edu>
2019-01-29 15:15:12 -0500

    added gitignore

--- project2/test.c
+++ project2/test.c
@@ -4,20 +4,20 @@
 
 #define MAX_CHARACTER_INPUT 1000
 #define MAX_DISTINCT_WORDS 	100
-#define BUF_CUSTOM 					10
 
 int main(int argc, char* argv[]){
 	// Read from line
-	char buf[BUF_CUSTOM];
-	char* text = calloc(1,1);
+	char buf[MAX_CHARACTER_INPUT];
+	char* words[MAX_DISTINCT_WORDS];
+
+	//char* text = calloc(1,1);
 	printf("Enter message: ");
-	while (fgets(buf, BUF_CUSTOM, stdin)){
-		text = realloc(text, strlen(text)+strlen(buf)+1);
+	while (fgets(buf, MAX_CHARACTER_INPUT, stdin)){
+		//text = realloc(text, strlen(text)+strlen(buf)+1);
 		
-		strcat(text, buf);
+		//strcat(text, buf);
 		printf("%s\n",buf);
 	}
-	printf("\nAll text:%s",text);
 
 	return EXIT_SUCCESS;
 }
