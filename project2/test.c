1561c82c0f0c719bbeb8a98c9f1667eaec687b6d
Matthew Phelps <mphelps3@nd.edu>
2019-01-29 14:48:27 -0500

    First includes and defines

--- project2/test.c
+++ project2/test.c
@@ -1,1 +1,10 @@
-#include <
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define MAX_CHARACTER_INPUT 1000
+#define MAX_DISTINCT_WORDS 	100
+
+int main(int argc, char* argv[]){
+	return EXIT_SUCCESS;
+}
