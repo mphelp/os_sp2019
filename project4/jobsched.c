5d593c0f0456a4135df49bdbbc977c3e7b360eec
Matthew Phelps <mphelps3@nd.edu>
2019-03-26 17:19:49 -0400

    wrote usage

--- project4/jobsched.c
+++ project4/jobsched.c
@@ -31,6 +31,40 @@
 */
 
 
+int helpFunc(char* words[]){
+	printf("Usage:\n");
+	printf("submit <command> - submit new job to be scheduled and run\n");
+	printf("status           - get status of all jobs\n");	
+	printf("wait <jobid>     - pause other jobs until chosen job finishes\n");
+	printf("remove <jobid>   - removes job from job queue if waited for or done\n");
+	printf("njobs <n>        - set number of jobs the scheduler can run at once\n");
+	printf("drain            - wait until all jobs are in done state\n");
+	printf("quit             - exit the program, regardless of jobs scheduled\n");
+	printf("help             - display usage\n");
+	return EXIT_SUCCESS;
+}
+int submitFunc(char* words[]){
+
+}
+int statusFunc(char* words[]){
+
+}
+int waitFunc(char* words[]){
+
+}
+int removeFunc(char* words[]){
+
+}
+int njobsFunc(char* words[]){
+
+}
+int drainFunc(char* words[]){
+
+}
+
+/* Command Functions */
+typedef int (*commandFunc)(char**);
+
 /* Command Line Parsing */
 void parseWordsFromLine(char* words[], char line[]){
 	words[0] = strtok(line," \t\n");
@@ -61,26 +95,26 @@ int main(int argc, char* argv[]){
 		if (fgets(line, MAX_CHARACTER_INPUT, stdin) != NULL){
 
 			parseWordsFromLine(words, line);
-			//commandFunc command;
+			commandFunc command;
 
 			// Retrieve program
 			// Probbaly could do for loop for specific commands and their commandFuncs
 			if (words[0] == NULL){ // spaces or enter key
 				continue;
 			} else if (streq(words[0], "help")){
-				/* command = &watchdogFunc; */
+				command = &helpFunc;
 			} else if (streq(words[0], "submit")){
-				/* command = &startFunc; */
+				command = &submitFunc;
 			} else if (streq(words[0], "status")){
-				/* command = &runFunc; */
+				command = &statusFunc;
 			} else if (streq(words[0], "wait")){
-				/* command = &waitFunc; */
+				command = &waitFunc;
 			} else if (streq(words[0], "remove")){
-				/* command = &waitforFunc; */
+				command = &removeFunc;
 			} else if (streq(words[0], "njobs")){
-				/* command = &watchdogFunc; */
+				command = &njobsFunc;
 			} else if (streq(words[0], "drain")){
-				/* command = &watchdogFunc; */
+				command = &drainFunc;
 			} else if (streq(words[0], "quit")){
 				return EXIT_SUCCESS;
 			} else {
@@ -88,10 +122,10 @@ int main(int argc, char* argv[]){
 				continue;
 			}
 			// Call command:
-			/* int commandReturn = (*command)(words); */
-			/* if (commandReturn == EXIT_FAILURE){ */
+			int commandReturn = (*command)(words);
+			if (commandReturn == EXIT_FAILURE){
 				// Optional space for general command failure error checking
-			/* } */
+			}
 		} else {
 			// Exit prompt
 			printf("\n");
